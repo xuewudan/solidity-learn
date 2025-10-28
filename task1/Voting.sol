f3d1a7780059bad64dbf4233f1772e1066ced9e5
xuetao <xuetaoxueliang@126.com>
2025-10-28 23:12:03 +0800

    Voting

---
+++ task1/Voting.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+contract Voting{
+
+string public name = "dongxiaojie";
+
+mapping ( address => uint256 ) public voters;
+
+
+
+
+
+
+
+
+}
