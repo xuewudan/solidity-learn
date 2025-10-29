fe601a045a8aefb574d5aeb0c8c4c7c25db0589f
xuetao <xuetaoxueliang@126.com>
2025-10-29 09:48:12 +0800

    format code

--- task1/Voting.sol
+++ task1/Voting.sol
@@ -1,17 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.30;
 
-contract Voting{
-
-string public name = "dongxiaojie";
-
-mapping ( address => uint256 ) public voters;
-
-
-
-
-
-
-
-
+contract Voting {
+    string public name = "dongxiaojie";
+    mapping(address => uint256) public voters;
 }
